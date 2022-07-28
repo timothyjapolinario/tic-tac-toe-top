63d4fb1a25316a44550ffd68c8216fa136e76596
JayJay <timothyjapolinario@gmail.com>
2022-07-28 10:59:52 +0800

    Change checking of winner algorithm

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const Player = (marker) => {
+const Player = (marker, board) => {
     let markedSlots = []
     const getMarker = () =>{
         return marker
@@ -13,33 +13,33 @@ const Player = (marker) => {
     const makeMove = (index) => {
         markedSlots.push(index)
     }
-    return {getMarker, makeMove, getMarkedSlots, removeAllMarks}
+    const makeMove2 = (index) =>{
+        board[index] = marker
+    }
+    return {getMarker, makeMove, makeMove2}
 }
 
 
 const Computer = (marker, difficulty,board) => {
     const prototype = Player(marker);
-    const markedSlots = prototype.getMarkedSlots()
     const speak = () => {
         console.log("beep-boop-beep-boop")
     }
 
     const makeMove = () =>{
         if(difficulty == 1){
-            randomMove()
+            return randomMove()
         }
     }
-
     const randomMove = () =>{
         
         let randomNumber = Math.floor(Math.random()*8)
-        console.log(board)
         if(board.includes(null)){
-            console.log("passed!")
             while(!(board[randomNumber] == null) ){
                 randomNumber = Math.floor(Math.random()*8)
             }
-            markedSlots.push(randomNumber)
+            board[randomNumber] = marker
+            return randomNumber
         }
     }
 
@@ -62,7 +62,7 @@ const Computer = (marker, difficulty,board) => {
 const Game = (() => {
     //let lastMoveWinner = ["x", "x", undefined,"x","o","x", "o", "x", "o"]
     let board = Array(9).fill(null)
-    let player_1 = Player("x")
+    let player_1 = Player("x", board)
     let player_2 = Computer("o", 1, board)
     let rounds = 9
     let currentPlayer = player_1
@@ -72,33 +72,25 @@ const Game = (() => {
     let resetButton = document.querySelector("#reset")
     let matchResult = document.querySelector(".match-result")
 
+
     const playTurn = (slot, index) =>{
-        player_1.makeMove(index)
-        updateBoard()
-        endTurn()
-        if(hasWinner||isDraw){
-            reset()
-            return
-        }
-        player_2.makeMove()
-        updateBoard()
-        renderBoard()
-        endTurn()
+        //Human Player
+        player_1.makeMove2(index)
+        endTurn(index, currentPlayer.getMarker())
         if(hasWinner||isDraw){
             reset()
             return
         }
         
+        //Computer Player
+        let computer_move = player_2.makeMove()
+        endTurn(computer_move, currentPlayer.getMarker())
+        if(hasWinner||isDraw){
+            reset()
+            return
+        }
     }
 
-    const updateBoard = () =>{
-        player_1.getMarkedSlots().forEach(slotIndex => {
-            board[slotIndex] = player_1.getMarker()
-        })
-        player_2.getMarkedSlots().forEach(slotIndex => {
-            board[slotIndex] = player_2.getMarker()
-        })
-    }
 
     const bindEvents = () =>{
     
@@ -133,13 +125,18 @@ const Game = (() => {
         })
     }
 
-    const endTurn = () =>{
+    const endTurn = (index, marker) =>{
         rounds -= 1
         renderBoard()
-        if(checkWinner()){
-            announceResult('Winner: ' + currentPlayer.getMarkedSlots())
-            return
+        if(getWinner(index, marker)){
+            hasWinner = true
+            announceResult('Winner: ' + currentPlayer.getMarker())
+            return 
         }
+        // if(checkWinner()){
+        //     announceResult('Winner: ' + currentPlayer.getMarkedSlots())
+        //     return
+        // }
         if(rounds == 0){
             announceResult('Draw!')
             isDraw = true;
@@ -147,50 +144,12 @@ const Game = (() => {
         }
         changeCurrentPlayer()
     }
-    const checkWinner = () =>{
-        const rows = [[0,1,2],[3,4,5],[6,7,8]]
-        const columns = [[0,3,6],[1,4,7],[2,5,8]]
-        const diagonals = [[0,4,8], [2,4,6]]
-        let checker = (arr, target) => target.every(v => arr.includes(v))
-        markedSlots = currentPlayer.getMarkedSlots()
-
-        //check rows
-        rows.forEach(row=>{
-            if(checker(markedSlots, row)){
-                hasWinner = true
-            }
-        })
-        if(hasWinner){
-            return hasWinner
-        }
-        //check column
-        columns.forEach(column=>{
-            if(checker(markedSlots, column)){
-                hasWinner = true
-            }
-        })
-        if(hasWinner){
-            return hasWinner
-        }
-        //check diagonal
-        diagonals.forEach(diagonal=>{
-            if(checker(markedSlots, diagonal)){
-                hasWinner = true
-            }
-        })
-        return hasWinner
-    }
 
     const getWinner = (square_index, new_letter) => {
         let row_index = Math.floor(square_index/3)*3
         let temp_row = board.slice(row_index, row_index+3)
-        let winning = temp_row.every(letter =>  {
-            console.log("lol")
-            return new_letter == letter
-        })
-        console.log(winning)
+        let winning = temp_row.every(letter => new_letter == letter)
         if(winning){
-            console.log('winning')
             return new_letter
         }
 
@@ -198,20 +157,20 @@ const Game = (() => {
         temp_col = board.filter((element, index) => {
             return (index % 3 == column_index)
         })
-        hasWinner = temp_col.every(letter =>  new_letter == letter)
-        if(hasWinner){
+        winning = temp_col.every(letter =>  new_letter == letter)
+        if(winning){
             return new_letter
         }
 
         let left_diagonal = [board[0],board[4],board[8]]
         let right_diagonal = [board[2],board[4],board[6]]
         
-        hasWinner = left_diagonal.every(letter =>  new_letter == letter)
-        if(hasWinner){
+        winning = left_diagonal.every(letter =>  new_letter == letter)
+        if(winning){
             return new_letter
         }
-        hasWinner = right_diagonal.every(letter =>  new_letter == letter)
-        if(hasWinner){
+        winning = right_diagonal.every(letter =>  new_letter == letter)
+        if(winning){
             return new_letter
         }
 
@@ -225,13 +184,8 @@ const Game = (() => {
     const removeResult = () => {
         matchResult.style.display = "none"
     }
-    const getBoard = () => {
-        return board
-    }
 
     let reset = () =>{
-        player_1.removeAllMarks()
-        player_2.removeAllMarks()
         hasWinner = false
         isDraw = false
         rounds = 9
